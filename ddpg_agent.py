44c3455e9e1f008fe360e70f9e065653442adf0d
Markus M. Geipel <markus.geipel@siemens.com>
2021-08-20 15:10:14 +0200

    extended report

--- ddpg_agent.py
+++ ddpg_agent.py
@@ -17,7 +17,7 @@ GAMMA = 0.95            # discount factor
 TAU = 1e-2              # for soft update of target parameters
 LR_ACTOR = 1e-4         # learning rate of the actor 
 LR_CRITIC = 3e-4        # learning rate of the critic
-GRAD_CLIP = 1
+GRAD_CLIP = 1           # clipping of gradient for critic
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
